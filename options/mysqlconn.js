9526b4adc427c3d2346209ec2d16bc0acf82bd78
Duvan Caicedo <duvamendi2@hotmail.com>
2022-08-07 18:11:56 -0500

    ajustes pg

--- options/mysqlconn.js
+++ options/mysqlconn.js
@@ -1,11 +1,19 @@
 const options1={
-    client:'mysql',
+    // client:'mysql',
+    // connection:{
+    //     host:'localhost',
+    //     port:3306,
+    //     user:'root',
+    //     password: '',
+    //     database: 'productos'      
+    // }
+    client:'pg',
     connection:{
-        host:'localhost',
-        port:3306,
-        user:'root',
-        password: '',
-        database: 'productos'      
+        host:'ec2-54-165-184-219.compute-1.amazonaws.com',
+        port:5432,
+        user:'fcitzrwsupprkg',
+        password: 'a3ea0b2cb251887be4d7faecb220a920e4979704931e113245254cd272bdd277',
+        database: 'd4eepe8h3fs00p'      
     }
 }
 module.exports =options1;
